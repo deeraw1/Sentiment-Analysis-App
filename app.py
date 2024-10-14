e308772ab7d5ab3ffaddb6c7c40d58c93a212440
deeraw1 <127483404+deeraw1@users.noreply.github.com>
2024-10-14 18:18:31 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import joblib
+import numpy as np
+ 
+model = joblib.load("hypertuned_tfidf_logreg_model.joblib")  # Load the pre-trained model
+print(model)
+
+st.title("Sentiment Review App")
+
+# Create a text input box for the user to enter text
+user_input = st.text_area("Enter your review:")
+
+# Predict the review when the user submits text
+if st.button('Check your Review'):
+    if user_input:
+        user_input = [user_input]
+        prediction = model.predict(user_input)  # Get model prediction
+        st.write(f"Review: {prediction}")
+    else:
+        st.write("Please enter a review text to analyze.")
